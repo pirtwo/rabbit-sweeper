2a4393fe5f957b7e7694847dc6f6215a8b6a868e
Ahmad Fz <ahmad.f.zadeh@icloud.com>
2020-08-04 10:13:55 +0430

    add cell size

--- src/sweeper.js
+++ src/sweeper.js
@@ -11,14 +11,13 @@ import {
 
 export default class Sweeper extends Container {
   constructor({
-    state = 1,
+    cellSize = 50,
     textures,
     cellLeftClicked,
     cellRightClicked
   }) {
     super();
-    this.state = state;
-    this.cellSize = 50;
+    this.cellSize = cellSize;
     this.grid = new Grid2D(10, 10);
     this.container = new Container();
     this.textures = textures;
@@ -36,14 +35,14 @@ export default class Sweeper extends Container {
 
     this.sfRabbit = () => {
       let sp = new Sprite(utils.getRandomElement([this.textures.rabbit, this.textures.carrot]));
-      sp.width = sp.height = 30;
+      sp.width = sp.height = this.cellSize / 2;
       sp.anchor.set(0.5, 0.5);
       return sp;
     }
 
     this.sfCell = () => {
       let sp = new Sprite(this.textures.ground);
-      sp.width = sp.height = 20;
+      sp.width = sp.height = this.cellSize / 3;
       sp.anchor.set(0.5, 0.5);
       return sp;
     }
@@ -53,8 +52,10 @@ export default class Sweeper extends Container {
     let cells = [],
       emptyCellNum = rows * cols - rabbits;
 
+    this.container.removeChildren(0);
+
     cells = new Array(rows * cols)
-      .fill().map((v, i) => new Cell(i < emptyCellNum ? 0 : 1, this.textures));
+      .fill().map((v, i) => new Cell(i < emptyCellNum ? 0 : 1, this.cellSize, this.textures));
     this.grid = new Grid2D(rows, cols, utils.shuffle(cells));
 
     for (const item of this.grid) {
@@ -171,9 +172,10 @@ export default class Sweeper extends Container {
 }
 
 class Cell extends Container {
-  constructor(value, textures) {
+  constructor(value, cellSize, textures) {
     super();
 
+    this.cellSize = cellSize;
     this.row = 0;
     this.col = 0;
     this.value = value; // zero = empty, one = rabbit
@@ -189,23 +191,26 @@ class Cell extends Container {
     this.ground = new Sprite(textures.ground);
     this.number = new Text(`${this.neighbourRabbits}`);
 
-    this.flag.width = this.ground.width / 2;
-    this.flag.height = this.ground.width / 2;
+    this.ground.width = this.cellSize;
+    this.ground.height = this.cellSize;
+
+    this.flag.width = this.cellSize / 2;
+    this.flag.height = this.cellSize / 2;
     this.flag.anchor.set(0.5, 0.5);
-    this.flag.position.set(this.ground.width / 2, this.ground.width / 2);
+    this.flag.position.set(this.cellSize / 2, this.cellSize / 2);
 
-    this.cross.width = this.ground.width / 2;
-    this.cross.height = this.ground.width / 2;
+    this.cross.width = this.cellSize / 2;
+    this.cross.height = this.cellSize / 2;
     this.cross.anchor.set(0.5, 0.5);
-    this.cross.position.set(this.ground.width / 2, this.ground.width / 2);
+    this.cross.position.set(this.cellSize / 2, this.cellSize / 2);
 
-    this.rabbit.width = this.ground.width / 2;
-    this.rabbit.height = this.ground.width / 2;
+    this.rabbit.width = this.cellSize / 2;
+    this.rabbit.height = this.cellSize / 2;
     this.rabbit.anchor.set(0.5, 0.5);
-    this.rabbit.position.set(this.ground.width / 2, this.ground.width / 2);
+    this.rabbit.position.set(this.cellSize / 2, this.cellSize / 2);
 
     this.number.anchor.set(0.5, 0.5);
-    this.number.position.set(this.ground.width / 2, this.ground.width / 2);
+    this.number.position.set(this.cellSize / 2, this.cellSize / 2);
 
     this.flag.visible = false;
     this.rabbit.visible = false;
@@ -237,6 +242,7 @@ class Cell extends Container {
 
     this.number.text = `${value}`;
     this.number.style = new TextStyle({
+      fontSize: this.cellSize / 2,
       fill: textColor,
     });
 
